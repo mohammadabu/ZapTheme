5a3da69d097b73b9ce49619ca77a9b6de62d0ab4
mohammadabu <55664960+mohammadabu@users.noreply.github.com>
2021-04-07 16:58:09 +0300

    edit

--- carryForwardsLeave/models/carryForwords.py
+++ carryForwardsLeave/models/carryForwords.py
@@ -8,3 +8,30 @@ from odoo.tools import email_split
 
 class HrLeave(models.Model):
     _inherit = 'hr.leave'
+
+
+
+class HrLeaveTypesCarryForwards(models.Model):
+    _inherit = 'hr.leave.type'
+    carry_forwards_validators = fields.One2many('hr.holidays.carry.forwards',
+                                       'hr_holiday_status',
+                                       string='Carry Forwards Validators', help="Carry Forwards validators")  
+
+
+
+class HrCarryForwardsValidators(models.Model):
+    _name = 'hr.holidays.carry.forwards'
+
+    hr_holiday_status = fields.Many2one('hr.leave.type')
+    validators_type = fields.Selection(
+        [
+            ('direct_manager','Direct Manager'),
+            ('position','Position'),
+            ('user','User')
+        ]
+    )
+    holiday_validators_user = fields.Many2one('res.users',
+                                         string='Leave Validators', help="Leave validators",
+                                         domain="[('share','=',False)]")
+    holiday_validators_position = fields.Many2one('hr.job')                                     
+    approval = fields.Boolean()                                              
