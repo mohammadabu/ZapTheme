33f1da25649883fcdabc4740d3132b6153e9e11d
mohammadabu <55664960+mohammadabu@users.noreply.github.com>
2021-04-12 15:27:39 +0300

    edit

---
+++ theme_zap/static/src/js/snip.js
@@ -0,0 +1,10 @@
+odoo.define('theme_zap.for_testing', function (require) {
+    'use strict';
+    var core = require('web.core');
+    var wUtils = require('website.utils');
+    var publicWidget = require('web.public.widget');
+    var _t = core._t;
+    console.log('yeeeeeees')
+
+})
+    
