86e40ecd60d9e29aae776fb507d04514cb6265ac
mohammadabu <55664960+mohammadabu@users.noreply.github.com>
2021-04-13 15:55:13 +0300

    Update snipOption.js

--- theme_zap/static/src/js/snipOption.js
+++ theme_zap/static/src/js/snipOption.js
@@ -6,28 +6,9 @@ odoo.define('theme_zap.snipOption', function (require) {
     var _t = core._t;
     console.log('test_1')
     sOptions.registry.js_get_test_limit = sOptions.Class.extend({
-        start: function () {
-            console.log('test_2')
-            // var def = this._rpc({
-            //     model: 'blog.blog',
-            //     method: 'search_read',
-            //     args: [wUtils.websiteDomain(this), ['name']],
-            // }).then(blogs => {
-            //     var allBlogsEl = this.el.querySelector('[data-filter-by-blog-id="0"]');
-            //     var menuEl = allBlogsEl.parentNode;
-            //     for (const blog of blogs) {
-            //         let el = allBlogsEl.cloneNode();
-            //         el.dataset.filterByBlogId = blog.id;
-            //         el.textContent = blog.name;
-            //         menuEl.appendChild(el);
-            //     }
-            //     this._setActive();
-            // });
-    
-            // return Promise.all([this._super.apply(this, arguments), def]);
-        },
         postsLimit: function (previewMode, value, $opt) {
             value = parseInt(value);
+            console.log('value')
             console.log(value)
             this.$target.attr('data-posts-test-limit', value).data('postsLimit', value);
             // this.trigger_up('widgets_start_request', {
