4bef14399afb83f0daac17137a548ee1d3b8cc64
mohammadabu <55664960+mohammadabu@users.noreply.github.com>
2021-04-13 16:07:51 +0300

    Update snipOption.js

--- theme_zap/static/src/js/snipOption.js
+++ theme_zap/static/src/js/snipOption.js
@@ -6,23 +6,16 @@ odoo.define('theme_zap.snipOption', function (require) {
     var _t = core._t;
     console.log('test_1')
     sOptions.registry.js_get_test_limit = sOptions.Class.extend({
-        postsLimit: function (previewMode, value, $opt) {
+        postsTestLimit: function (previewMode, value, $opt) {
             value = parseInt(value);
             console.log('value')
             console.log(value)
-            this.$target.attr('data-posts-test-limit', value).data('postsLimit', value);
-            // this.trigger_up('widgets_start_request', {
-            //     editableMode: true,
-            //     $target: this.$target,
-            // });
+            this.$target.attr('data-posts-test-limit', value).data('postsTestLimit', value);
+            this.trigger_up('widgets_start_request', {
+                editableMode: true,
+                $target: this.$target,
+            });
 
-        },
-        _setActive: function () {
-            this._super.apply(this, arguments);
-            var activeLimit = this.$target.data('postsLimit') || 3;
-    
-            this.$el.find('[data-posts-limit]').removeClass('active');
-            this.$el.find('[data-posts-limit=' + activeLimit + ']').addClass('active');
         },
     })
 })    
