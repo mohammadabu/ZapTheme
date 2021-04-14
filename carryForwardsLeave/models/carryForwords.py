3da1d929808fee4bbc9532c5f9468733fee6b9d0
mohammadabu <55664960+mohammadabu@users.noreply.github.com>
2021-04-15 02:42:11 +0300

    Update carryForwords.py

--- carryForwardsLeave/models/carryForwords.py
+++ carryForwardsLeave/models/carryForwords.py
@@ -57,7 +57,9 @@ class HrLeaveTypesCarryForwards(models.Model):
         # ,'&',('validity_stop','>',datetime.strptime(str(datetime.today()),'%Y-%m-%d')
         annual_leave_type = self.env['hr.leave.type'].sudo().search([('carry_forwards','=','True'),('finished_carry_froword','=',False)])
         for annual in annual_leave_type:
-            _logger.info(annual.validity_stop)
+            current_date = str(datetime.now().date())
+            if annual.validity_stop > current_date
+                _logger.info(annual.validity_stop)
 
 
 class HrCarryForwardsValidators(models.Model):
