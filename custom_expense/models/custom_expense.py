76e8ad67d234bd8b7e4be6fc97998799930354de
mohammadabu <55664960+mohammadabu@users.noreply.github.com>
2021-05-27 11:39:04 +0300

    Update custom_expense.py

--- custom_expense/models/custom_expense.py
+++ custom_expense/models/custom_expense.py
@@ -38,3 +38,52 @@ class HrExpenseSheet(models.Model):
         ('done', 'Paid'),
         ('cancel', 'Refused')
     ], string='Status', index=True, readonly=True, tracking=True, copy=False, default='draft', required=True, help='Expense Report State') 
+
+
+    def approve_expense_direct(self):
+        # if not self.user_has_groups('hr_expense.group_hr_expense_team_approver'):
+        #     raise UserError(_("Only Managers and HR Officers can approve expenses"))
+        # elif not self.user_has_groups('hr_expense.group_hr_expense_manager'):
+        #     current_managers = self.employee_id.expense_manager_id | self.employee_id.parent_id.user_id | self.employee_id.department_id.manager_id.user_id
+
+        #     if self.employee_id.user_id == self.env.user:
+        #         raise UserError(_("You cannot approve your own expenses"))
+
+        #     if not self.env.user in current_managers and not self.user_has_groups('hr_expense.group_hr_expense_user') and self.employee_id.expense_manager_id != self.env.user:
+        #         raise UserError(_("You can only approve your department expenses"))
+
+        responsible_id = self.user_id.id or self.env.user.id
+        self.write({'state': 'direct', 'user_id': responsible_id})
+        self.activity_update()
+
+    def approve_expense_manager_of_manager(self):
+        # if not self.user_has_groups('hr_expense.group_hr_expense_team_approver'):
+        #     raise UserError(_("Only Managers and HR Officers can approve expenses"))
+        # elif not self.user_has_groups('hr_expense.group_hr_expense_manager'):
+        #     current_managers = self.employee_id.expense_manager_id | self.employee_id.parent_id.user_id | self.employee_id.department_id.manager_id.user_id
+
+        #     if self.employee_id.user_id == self.env.user:
+        #         raise UserError(_("You cannot approve your own expenses"))
+
+        #     if not self.env.user in current_managers and not self.user_has_groups('hr_expense.group_hr_expense_user') and self.employee_id.expense_manager_id != self.env.user:
+        #         raise UserError(_("You can only approve your department expenses"))
+
+        responsible_id = self.user_id.id or self.env.user.id
+        self.write({'state': 'manager_of_manager', 'user_id': responsible_id})
+        self.activity_update()  
+
+    def approve_expense_manager_of_manager(self):
+        # if not self.user_has_groups('hr_expense.group_hr_expense_team_approver'):
+        #     raise UserError(_("Only Managers and HR Officers can approve expenses"))
+        # elif not self.user_has_groups('hr_expense.group_hr_expense_manager'):
+        #     current_managers = self.employee_id.expense_manager_id | self.employee_id.parent_id.user_id | self.employee_id.department_id.manager_id.user_id
+
+        #     if self.employee_id.user_id == self.env.user:
+        #         raise UserError(_("You cannot approve your own expenses"))
+
+        #     if not self.env.user in current_managers and not self.user_has_groups('hr_expense.group_hr_expense_user') and self.employee_id.expense_manager_id != self.env.user:
+        #         raise UserError(_("You can only approve your department expenses"))
+
+        responsible_id = self.user_id.id or self.env.user.id
+        self.write({'state': 'approve', 'user_id': responsible_id})
+        self.activity_update()        
