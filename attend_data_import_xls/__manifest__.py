c22a3782dca0247c1a2b54c006065bb53319883f
mohammadabu <55664960+mohammadabu@users.noreply.github.com>
2021-06-14 17:05:16 +0300

    ed

--- attend_data_import_xls/__manifest__.py
+++ attend_data_import_xls/__manifest__.py
@@ -3,7 +3,7 @@
     'version': '13.0.0.1.0',
     'depends': ["hr_attendance"],
     'data': [
-        # 'wizard/import_part_data_view.xml',
+        'wizard/import_attendances_data_view.xml',
         'views/import_attendances_history_view.xml',
         'views/import_attendances_master_view.xml',
         'security/ir.model.access.csv',
