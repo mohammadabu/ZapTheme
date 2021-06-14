c86449668567d21be498c5f4be8a568871dcf7eb
mohammadabu <55664960+mohammadabu@users.noreply.github.com>
2021-06-14 15:30:40 +0300

    Update __manifest__.py

--- attend_data_import_xls/__manifest__.py
+++ attend_data_import_xls/__manifest__.py
@@ -1,7 +1,7 @@
 {
     'name': 'Attend Data Import - XLS',
     'version': '13.0.0.1.0',
-    'depends': ["hr_attendance", "hr_holidays_public"],
+    'depends': ["hr_attendance"],
     'data': [
         # 'wizard/import_part_data_view.xml',
         'views/import_part_history_view.xml',
