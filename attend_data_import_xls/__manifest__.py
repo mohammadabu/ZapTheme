945a07cb0d79abba165f3d96097af532d0244185
mohammadabu <55664960+mohammadabu@users.noreply.github.com>
2021-06-14 15:29:27 +0300

    ed

---
+++ attend_data_import_xls/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Attend Data Import - XLS',
+    'version': '13.0.0.1.0',
+    'depends': ["hr_attendance", "hr_holidays_public"],
+    'data': [
+        # 'wizard/import_part_data_view.xml',
+        'views/import_part_history_view.xml',
+        # 'views/import_part_master_view.xml',
+        # 'security/ir.model.access.csv',
+        # 'data/data.xml',
+    ],
+}
