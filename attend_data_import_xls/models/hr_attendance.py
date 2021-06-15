f675968dac23d4e964f309c5ea7990f34709798d
mohammadabu <55664960+mohammadabu@users.noreply.github.com>
2021-06-15 11:28:16 +0300

    Update hr_attendance.py

--- attend_data_import_xls/models/hr_attendance.py
+++ attend_data_import_xls/models/hr_attendance.py
@@ -92,22 +92,21 @@ class ResPartner(models.Model):
                                 if emp_name != "" and (check_in != "" or check_out != ""):
                                     _logger.info("------------------------")  
                                     if check_in != "":
-                                        split_check_in = check_in.split(" ")
-                                        split_check_in_0 = split_check_in[0]
-                                        split_check_in_0 = split_check_in_0.encode("ascii","ignore")
-                                        split_check_in_0 = split_check_in_0.decode()
+                                        # split_check_in = check_in.split(" ")
+                                        # split_check_in_0 = split_check_in[0]
+                                        # split_check_in_0 = split_check_in_0.encode("ascii","ignore")
+                                        # split_check_in_0 = split_check_in_0.decode()
                                         
-                                        split_check_in_1 = split_check_in[1]
-                                        split_check_in_1 = split_check_in_1.encode("ascii","ignore")
-                                        split_check_in_1 = split_check_in_1.decode()
-                                        _logger.info(split_check_in_0)
-                                        _logger.info(split_check_in_1)
-                                        
-                                    _logger.info(emp_name)  
-                                    _logger.info(date)  
-                                    _logger.info(check_in)  
-                                    _logger.info(check_out)  
-                                    _logger.info("------------------------") 
+                                        # split_check_in_1 = split_check_in[1]
+                                        # split_check_in_1 = split_check_in_1.encode("ascii","ignore")
+                                        # split_check_in_1 = split_check_in_1.decode()
+                                        # _logger.info(split_check_in_0)
+                                        # _logger.info(split_check_in_1)
+                                        _logger.info(emp_name)  
+                                        _logger.info(date)  
+                                        _logger.info(check_in)  
+                                        _logger.info(check_out)  
+                                        _logger.info("------------------------") 
                     #         if rownum == 0:
                     #             header_list = [
                     #                 x for x in sheet.row_values(rownum)]
