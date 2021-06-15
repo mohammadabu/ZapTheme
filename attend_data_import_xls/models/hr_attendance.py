8f52460bed57e63664f60cce8f45d8153ac5f127
mohammadabu <55664960+mohammadabu@users.noreply.github.com>
2021-06-15 11:58:57 +0300

    Update hr_attendance.py

--- attend_data_import_xls/models/hr_attendance.py
+++ attend_data_import_xls/models/hr_attendance.py
@@ -94,8 +94,8 @@ class ResPartner(models.Model):
                                         split_check_in = check_in.split(" ")
                                         if len(split_check_in) > 1:
                                             _logger.info("------------------------")
-                                            # check_in_time = split_check_in[0]
-                                            # check_in_zone = split_check_in[1]
+                                            check_in_time = split_check_in[0]
+                                            check_in_zone = split_check_in[1]
                                             # split_check_in_0 = split_check_in[0]
                                             # split_check_in_0 = split_check_in_0.encode("ascii","ignore")
                                             # split_check_in_0 = split_check_in_0.decode()
@@ -107,10 +107,10 @@ class ResPartner(models.Model):
                                             # _logger.info(split_check_in_1)
                                             # _logger.info(emp_name)  
                                             # _logger.info(date) 
-                                            # _logger.info(check_in_time)   
-                                            # _logger.info(check_in_zone)  
-                                            _logger.info(split_check_in)
-                                            _logger.info(check_in)  
+                                            _logger.info(check_in_time)   
+                                            _logger.info(check_in_zone)  
+                                            # _logger.info(split_check_in)
+                                            # _logger.info(check_in)  
                                             _logger.info("------------------------") 
                     #         if rownum == 0:
                     #             header_list = [
