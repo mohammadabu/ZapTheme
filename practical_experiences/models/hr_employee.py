f6f01fb2f5af7bd94853225a4a004d7931681ae6
mohammadabu <55664960+mohammadabu@users.noreply.github.com>
2021-06-22 11:43:47 +0300

    ed

--- practical_experiences/models/hr_employee.py
+++ practical_experiences/models/hr_employee.py
@@ -8,9 +8,22 @@ from xlrd import open_workbook
 import tempfile
 _logger = logging.getLogger(__name__)
 
-
-
-
+class PracticalExperiences(models.Model):
+    _name = "hr.employee.practical.experiences"
+    name = fields.Char()
+    practical_experiences = fields.Selection(
+        [
+            ("training_courses","Training courses"),
+            ("certificates","Certificates"),
+            ("cv","CV"),
+            ("experiences","Experiences")
+        ]
+    )
+    form_date = fields.Date()
+    to_date = fields.Date()
+    attachment = fields.Binary()
+    summary = fields.Html()
+    employee_id = fields.Many2one('hr.employee',default=lambda self: self.env.context.get('active_id', []))
 
 
 class CustomHrEmployee(models.Model):
