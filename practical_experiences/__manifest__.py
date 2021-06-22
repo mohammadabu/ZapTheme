fb67fd402577e8d67e32326dc040532c88282962
mohammadabu <55664960+mohammadabu@users.noreply.github.com>
2021-06-22 11:47:16 +0300

    Update __manifest__.py

--- practical_experiences/__manifest__.py
+++ practical_experiences/__manifest__.py
@@ -1,7 +1,7 @@
 {
     'name': 'practical_experiences',
     'version': '13.0.0.1.0',
-    'depends': ["hr_employee"],
+    'depends': ["hr"],
     'data': [
         # 'views/custom_hr_employee.xml',
         'security/ir.model.access.csv'
