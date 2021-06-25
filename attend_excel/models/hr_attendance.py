60c8b194a96e9a846258c19cba4ba03f670ee10d
mohammadabu <55664960+mohammadabu@users.noreply.github.com>
2021-06-25 17:53:48 +0300

    Update hr_attendance.py

--- attend_excel/models/hr_attendance.py
+++ attend_excel/models/hr_attendance.py
@@ -103,6 +103,12 @@ class AttendanceReportExcel(models.TransientModel):
         all_employee_attendance =  self.pool.get("wizard.attendance.history.excel").get_absent_days(self,employee_id,from_date,to_date)
         # _logger.info('all_employee_attendance')
         # _logger.info(all_employee_attendance)
+        if all_employee_attendance['absent_days'] == False:
+            all_employee_attendance['absent_days'] = 0
+        if all_employee_attendance['absent_days_without_leave'] == False:
+            all_employee_attendance['absent_days_without_leave'] = 0
+        if all_employee_attendance['late_hours'] == False:
+            all_employee_attendance['late_hours'] = 0        
         table_excel = {}
         table_excel['id_number'] = id_number
         table_excel['employee_name'] = employee_name
