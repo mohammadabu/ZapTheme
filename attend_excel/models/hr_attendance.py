cfa2b64f1e70df73a11ce8c099a3efaa8b357848
mohammadabu <55664960+mohammadabu@users.noreply.github.com>
2021-06-25 18:18:07 +0300

    Update hr_attendance.py

--- attend_excel/models/hr_attendance.py
+++ attend_excel/models/hr_attendance.py
@@ -234,7 +234,7 @@ class AttendanceReportExcel(models.TransientModel):
         cell_format.set_text_wrap()
         sheet.merge_range('C1:I3', "", cell_format)
         header_excel = "التقرير الشامل - أيام الغياب وساعات العمل"
-        header_excel += "\ من 1442/09/20-2021/05/02 الى 1442/10/19-2021/05/31"
+        header_excel += " \n من 1442/09/20-2021/05/02 الى 1442/10/19-2021/05/31"
         sheet.write('C1',header_excel,cell_format)
         sheet.set_column('A:A', 1)
         sheet.set_column('B:B', 1)
