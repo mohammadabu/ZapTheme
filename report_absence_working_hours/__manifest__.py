efe0a0a56edc9da458ba3e855847696519f24a6e
mohammadabu <55664960+mohammadabu@users.noreply.github.com>
2021-06-25 19:17:35 +0300

    ed

---
+++ report_absence_working_hours/__manifest__.py
@@ -0,0 +1,8 @@
+{
+    'name': 'Report for absence and working hours',
+    'version': '13.0.0.1.0',
+    'depends': ["hr_attendance"],
+    'data': [
+        'views/hr_attendance_excel.xml'
+    ],
+}
