59df05f07422f1b81e7acdd60a9598344a1d22a5
mohammadabu <55664960+mohammadabu@users.noreply.github.com>
2021-06-26 15:24:00 +0300

    Update hr_employee.py

--- docs_odoo/models/hr_employee.py
+++ docs_odoo/models/hr_employee.py
@@ -7,6 +7,7 @@ import json
 import io
 from odoo import api, fields, models, _
 import logging
+from docx import Document
 from odoo.tools import date_utils
 from hijri_converter import convert
 _logger = logging.getLogger(__name__)
@@ -24,37 +25,38 @@ class HrEmployeeDocuments(models.Model):
 
 
     def generate_salary_definition_form(self):
-        self.pool.get("wizard.hr.employee.history.docs").generate_salary_definition_form(self)
+        _logger.info('dsadasdads')
+        # self.pool.get("wizard.hr.employee.history.docs").generate_salary_definition_form(self)
 
 
 
 
-class StockReport(models.TransientModel):
-    _name = "wizard.hr.employee.history.docs"
-    _description = "Current Docs History"
+# class StockReport(models.TransientModel):
+#     _name = "wizard.hr.employee.history.docs"
+#     _description = "Current Docs History"
 
 
-    @api.model
-    def generate_salary_definition_form(self):
-        data = {
-            'ids': self.ids,
-            'model': self._name
-        }
-        return {
-            'type': 'ir_actions_docs_download',
-            'data': {'model': 'wizard.hr.employee.history.docs',
-                     'options': json.dumps(data, default=date_utils.json_default),
-                     'output_format': 'docx',
-                     'report_name': 'التقرير الشامل لأيام الغياب وساعات العمل',
-                    }
-        }    
+#     @api.model
+#     def generate_salary_definition_form(self):
+#         data = {
+#             'ids': self.ids,
+#             'model': self._name
+#         }
+#         return {
+#             'type': 'ir_actions_docs_download',
+#             'data': {'model': 'wizard.hr.employee.history.docs',
+#                      'options': json.dumps(data, default=date_utils.json_default),
+#                      'output_format': 'docx',
+#                      'report_name': 'التقرير الشامل لأيام الغياب وساعات العمل',
+#                     }
+#         }    
 
-    def get_docs_report(self, data, response):
-        output = io.BytesIO()
-        # lines = self.browse(data['ids'])
-        # workbook = xlsxwriter.Workbook(output, {'in_memory': True})
-        _logger.info('test test tes n3 n32')
-        # workbook.close()
-        output.seek(0)
-        response.stream.write(output.read())
-        output.close()    
+#     def get_docs_report(self, data, response):
+#         output = io.BytesIO()
+#         # lines = self.browse(data['ids'])
+#         # workbook = xlsxwriter.Workbook(output, {'in_memory': True})
+#         _logger.info('test test tes n3 n32')
+#         # workbook.close()
+#         output.seek(0)
+#         response.stream.write(output.read())
+#         output.close()    
