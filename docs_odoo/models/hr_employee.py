42a889cd38a109e838a774a6bc267a7e9805c6ec
mohammadabu <55664960+mohammadabu@users.noreply.github.com>
2021-06-26 14:19:40 +0300

    Update hr_employee.py

--- docs_odoo/models/hr_employee.py
+++ docs_odoo/models/hr_employee.py
@@ -1,4 +1,19 @@
-from odoo import models, fields, api
+import time
+from datetime import date, datetime,timedelta
+from calendar import monthrange
+from odoo.addons.resource.models.resource import float_to_time, HOURS_PER_DAY
+import pytz
+import json
+import io
+from odoo import api, fields, models, _
+import logging
+from odoo.tools import date_utils
+from hijri_converter import convert
+_logger = logging.getLogger(__name__)
+try:
+    from odoo.tools.misc import xlsxwriter
+except ImportError:
+    import xlsxwriter
 
 class HrEmployeeDocuments(models.Model):
     _inherit = 'hr.employee'
@@ -6,3 +21,7 @@ class HrEmployeeDocuments(models.Model):
     salary_definition = fields.Char()
 
     job_definition = fields.Char()
+
+
+    def generate_salary_definition_form(self):
+        _logger.info('test test ')
