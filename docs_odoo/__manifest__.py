44894c627385e83a360c4a99e7baa01e2455078a
mohammadabu <55664960+mohammadabu@users.noreply.github.com>
2021-06-27 13:28:10 +0300

    ed

--- docs_odoo/__manifest__.py
+++ docs_odoo/__manifest__.py
@@ -3,6 +3,7 @@
     'version': '13.0.0.1.0',
     'depends': ["hr"],
     'data': [
+        'data/sale_order_report_data.xml',
         'views/action_manager.xml',
         'views/hr_employee.xml'
     ],
