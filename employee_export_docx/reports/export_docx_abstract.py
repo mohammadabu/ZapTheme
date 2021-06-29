0fb435e8d033414694c7205281964bcb6882fbcc
mohammadabu <55664960+mohammadabu@users.noreply.github.com>
2021-06-29 22:38:41 +0300

    Update export_docx_abstract.py

--- employee_export_docx/reports/export_docx_abstract.py
+++ employee_export_docx/reports/export_docx_abstract.py
@@ -52,45 +52,55 @@ class ExportDocxAbstract(models.AbstractModel):
         # font.size = Pt(18)
 
 
+        # document = Document()
+
+        # document.add_heading('Document Title', 0)
+
+        # p = document.add_paragraph('A plain paragraph having some ')
+        # p.add_run('bold').bold = True
+        # p.add_run(' and some ')
+        # p.add_run('italic.').italic = True
+
+        # document.add_heading('Heading, level 1', level=1)
+        # document.add_paragraph('Intense quote', style='Intense Quote')
+
+        # document.add_paragraph(
+        #     'first item in unordered list', style='List Bullet'
+        # )
+        # document.add_paragraph(
+        #     'first item in ordered list', style='List Number'
+        # )
+
+        # # document.add_picture('monty-truth.png', width=Inches(1.25))
+
+        # records = (
+        #     (3, '101', 'Spam'),
+        #     (7, '422', 'Eggs'),
+        #     (4, '631', 'Spam, spam, eggs, and spam')
+        # )
+
+        # table = document.add_table(rows=1, cols=3)
+        # hdr_cells = table.rows[0].cells
+        # hdr_cells[0].text = 'Qty'
+        # hdr_cells[1].text = 'Id'
+        # hdr_cells[2].text = 'Desc'
+        # for qty, id, desc in records:
+        #     row_cells = table.add_row().cells
+        #     row_cells[0].text = str(qty)
+        #     row_cells[1].text = id
+        #     row_cells[2].text = desc
+
+        # document.add_page_break()
+
+
         document = Document()
+        section = document.sections[0]
 
-        document.add_heading('Document Title', 0)
+        sectPr = section._sectPr
+        cols = sectPr.xpath('./w:cols')[0]
+        cols.set(qn('w:num'),'2')
 
-        p = document.add_paragraph('A plain paragraph having some ')
-        p.add_run('bold').bold = True
-        p.add_run(' and some ')
-        p.add_run('italic.').italic = True
 
-        document.add_heading('Heading, level 1', level=1)
-        document.add_paragraph('Intense quote', style='Intense Quote')
-
-        document.add_paragraph(
-            'first item in unordered list', style='List Bullet'
-        )
-        document.add_paragraph(
-            'first item in ordered list', style='List Number'
-        )
-
-        # document.add_picture('monty-truth.png', width=Inches(1.25))
-
-        records = (
-            (3, '101', 'Spam'),
-            (7, '422', 'Eggs'),
-            (4, '631', 'Spam, spam, eggs, and spam')
-        )
-
-        table = document.add_table(rows=1, cols=3)
-        hdr_cells = table.rows[0].cells
-        hdr_cells[0].text = 'Qty'
-        hdr_cells[1].text = 'Id'
-        hdr_cells[2].text = 'Desc'
-        for qty, id, desc in records:
-            row_cells = table.add_row().cells
-            row_cells[0].text = str(qty)
-            row_cells[1].text = id
-            row_cells[2].text = desc
-
-        document.add_page_break()
 
 
         path_docx = path_docx + '/EmployeeDocx_' + timestamp + ".docx"
