4bb3696dae6e2c024d8b4f1f2db4cc9676aaa6ac
mohammadabu <55664960+mohammadabu@users.noreply.github.com>
2021-06-29 22:49:13 +0300

    Update export_docx_abstract.py

--- employee_export_docx/reports/export_docx_abstract.py
+++ employee_export_docx/reports/export_docx_abstract.py
@@ -93,13 +93,35 @@ class ExportDocxAbstract(models.AbstractModel):
         # document.add_page_break()
 
 
-        document = Document()
-        section = document.sections[0]
+        # document = Document()
+        # section = document.sections[0]
 
-        sectPr = section._sectPr
-        cols = sectPr.xpath('./w:cols')[0]
-        cols.set(qn('w:num'),'2')
+        # sectPr = section._sectPr
+        # cols = sectPr.xpath('./w:cols')[0]
+        # cols.set(qn('w:num'),'2')
 
+
+        document = docx.Document()
+
+        document.add_heading('Table Document',0)
+
+        records = [
+            [1, 'Chicken Pasta Bake', '£4.50'],
+            [2, 'Starfish Cakes', '£5.00'],
+            [3, 'Cod Confit', '£2.75']
+                ]
+        menuTable = document.add_table(rows=1,cols=3)
+        menuTable.style= 'Medium Shading 2 Accent 3'
+        hdr_Cells = menuTable.rows[0].cells
+        hdr_Cells[0].text = 'ID'
+        hdr_Cells[1].text = 'Meal Name'
+        hdr_Cells[2].text = 'Price'
+
+        for ID, nameOfMeal, price in records:
+            row_Cells = menuTable.add_row().cells
+            row_Cells[0].text= str(ID)
+            row_Cells[1].text = nameOfMeal
+            row_Cells[2].text = price
 
 
 
