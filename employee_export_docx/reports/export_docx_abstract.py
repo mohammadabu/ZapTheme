340c32fa4715adc66fd7e218bdd0d1b8e404d066
mohammadabu <55664960+mohammadabu@users.noreply.github.com>
2021-06-29 22:28:14 +0300

    Update export_docx_abstract.py

--- employee_export_docx/reports/export_docx_abstract.py
+++ employee_export_docx/reports/export_docx_abstract.py
@@ -44,12 +44,53 @@ class ExportDocxAbstract(models.AbstractModel):
         timestamp = str(int(datetime.timestamp(datetime.now())))
         path_docx = '/var/lib/odoo/.local/share/Odoo/'
 
+        # document = Document()
+        # paragraph = document.add_paragraph('Python is cool')
+        # paragraph.style = document.styles.add_style('', WD_STYLE_TYPE.PARAGRAPH)
+        # font = paragraph.style.font
+        # font.name = 'Times New Roman'
+        # font.size = Pt(18)
+
+
         document = Document()
-        paragraph = document.add_paragraph('Python is cool')
-        paragraph.style = document.styles.add_style('', WD_STYLE_TYPE.PARAGRAPH)
-        font = paragraph.style.font
-        font.name = 'Times New Roman'
-        font.size = Pt(18)
+
+        document.add_heading('Document Title', 0)
+
+        p = document.add_paragraph('A plain paragraph having some ')
+        p.add_run('bold').bold = True
+        p.add_run(' and some ')
+        p.add_run('italic.').italic = True
+
+        document.add_heading('Heading, level 1', level=1)
+        document.add_paragraph('Intense quote', style='Intense Quote')
+
+        document.add_paragraph(
+            'first item in unordered list', style='List Bullet'
+        )
+        document.add_paragraph(
+            'first item in ordered list', style='List Number'
+        )
+
+        document.add_picture('monty-truth.png', width=Inches(1.25))
+
+        records = (
+            (3, '101', 'Spam'),
+            (7, '422', 'Eggs'),
+            (4, '631', 'Spam, spam, eggs, and spam')
+        )
+
+        table = document.add_table(rows=1, cols=3)
+        hdr_cells = table.rows[0].cells
+        hdr_cells[0].text = 'Qty'
+        hdr_cells[1].text = 'Id'
+        hdr_cells[2].text = 'Desc'
+        for qty, id, desc in records:
+            row_cells = table.add_row().cells
+            row_cells[0].text = str(qty)
+            row_cells[1].text = id
+            row_cells[2].text = desc
+
+        document.add_page_break()
 
 
         path_docx = path_docx + '/EmployeeDocx_' + timestamp + ".docx"
