7f8bb9dd855918bc1738ee681de894193d0d43d9
mohammadabu <55664960+mohammadabu@users.noreply.github.com>
2021-07-03 15:28:30 +0300

    Update export_docx_abstract.py

--- employee_export_docx/reports/export_docx_abstract.py
+++ employee_export_docx/reports/export_docx_abstract.py
@@ -150,6 +150,19 @@ class ExportDocxAbstract(models.AbstractModel):
         
 
 
+        row_Cells = menuTable.add_row().cells
+        row_Cells[0].width = Cm(7.82)
+        row_Cells[1].width = Cm(0.87)
+        
+        row_Cells[2].width = Cm(9.3)
+        row_Cells[2].text = "مدير الموارد البشرية"
+        paragraph_row_2 = row_Cells[2].paragraphs[0]
+        run_row_2 = paragraph_row_2.runs
+        font_row_2 = run_row_2[0].font
+        font_row_2.size= Pt(16)
+        font_row_2.bold = True
+        font_row_2.name = 'Sakkal Majalla'
+        paragraph_row_2.alignment = WD_PARAGRAPH_ALIGNMENT.LIFT
 
 
         path_docx = path_docx + '/EmployeeDocx_' + timestamp + ".docx"
