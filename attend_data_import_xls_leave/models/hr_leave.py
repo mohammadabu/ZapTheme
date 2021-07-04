c4a89d40b4722b2e358c35738174feb80fa5baaf
mohammadabu <55664960+mohammadabu@users.noreply.github.com>
2021-07-04 17:16:12 +0300

    Update hr_leave.py

--- attend_data_import_xls_leave/models/hr_leave.py
+++ attend_data_import_xls_leave/models/hr_leave.py
@@ -93,6 +93,7 @@ class ImportHrLeave(models.Model):
                     # file_data = file_data.replace('<html dir="rtl">',"",100)
                     # file_data = file_data.encode()
                     _logger.info("file_data")
+                    file_data = file_data.replace(/<[^>]+>/g,"")
                     _logger.info(file_data)
                     # fp = open(temp_path + '/xsl_file.xls', 'wb+')
                     # fp.write(file_data)
