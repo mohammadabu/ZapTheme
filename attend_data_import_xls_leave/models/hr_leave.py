f284c5784e3d64f9d71c86085aa244572b407c00
mohammadabu <55664960+mohammadabu@users.noreply.github.com>
2021-07-04 23:31:46 +0300

    Update hr_leave.py

--- attend_data_import_xls_leave/models/hr_leave.py
+++ attend_data_import_xls_leave/models/hr_leave.py
@@ -7,10 +7,13 @@ _logger = logging.getLogger(__name__)
 class ImportHrLeave(models.Model):
     _inherit = 'hr.leave'        
     def import_data(self, part_master_id=False):
-        datafile = part_master.file
-        temp_path = tempfile.gettempdir()
-        file_data = base64.decodestring(datafile)
-        fp = open(temp_path + '/xsl_file.xls', 'wb+')
-        fp.write(file_data)
-        fp.close()
-        wb = open_workbook(temp_path + '/xsl_file.xls')
+        if part_master_id:
+            part_master = self.env[
+                'import.leave.master'].browse(part_master_id)
+            datafile = part_master.file
+            temp_path = tempfile.gettempdir()
+            file_data = base64.decodestring(datafile)
+            fp = open(temp_path + '/xsl_file.xls', 'wb+')
+            fp.write(file_data)
+            fp.close()
+            wb = open_workbook(temp_path + '/xsl_file.xls')
