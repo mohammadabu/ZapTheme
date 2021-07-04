50c8149d3221a6d607982b7e8d86a08cff19ee91
mohammadabu <55664960+mohammadabu@users.noreply.github.com>
2021-07-04 14:51:35 +0300

    ed

---
+++ attend_data_import_xls_leave/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Attend Data Import - XLS - Leave',
+    'version': '13.0.0.1.0',
+    'depends': ["hr_holiday","web_notify"],
+    'data': [
+        # 'views/hr_attendance.xml',
+        'wizard/import_leave_data_view.xml',
+        # 'views/import_attendances_history_view.xml',
+        # 'views/import_attendances_master_view.xml',
+        # 'security/ir.model.access.csv',
+        # 'data/data.xml', 
+    ],
+}
