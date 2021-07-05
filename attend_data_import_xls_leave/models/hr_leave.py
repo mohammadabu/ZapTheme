f0a0019e19951985daacd668b0413c483c51ba68
mohammadabu <55664960+mohammadabu@users.noreply.github.com>
2021-07-05 11:49:23 +0300

    Update hr_leave.py

--- attend_data_import_xls_leave/models/hr_leave.py
+++ attend_data_import_xls_leave/models/hr_leave.py
@@ -150,7 +150,7 @@ class ImportHrLeave(models.Model):
                                 start_date = item_y[start_date_row]
                                 end_date = item_y[end_date_row]
                                 _logger.info(first_row)
-                                _logger.info(emp_num)
+                                _logger.info(int(emp_num))
                                 _logger.info(type_leave)
                                 _logger.info(duration)
                                 _logger.info(start_date)
