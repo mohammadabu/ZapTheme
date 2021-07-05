fec0640a49cb88a5315d9e4462cc26fc37c2ac02
mohammadabu <55664960+mohammadabu@users.noreply.github.com>
2021-07-05 12:42:49 +0300

    Update hr_leave.py

--- attend_data_import_xls_leave/models/hr_leave.py
+++ attend_data_import_xls_leave/models/hr_leave.py
@@ -95,82 +95,96 @@ class ImportHrLeave(models.Model):
                     fp.write(file_data)
                     fp.close()
                     wb = open_workbook(temp_path + '/xsl_file.xls')
-                    data_list = []
-                    header_list = []
-                    headers_dict = {}
-                    for sheet in wb.sheets():
-                        _logger.info(sheet.nrows)
-                        first_row = False
-                        emp_num_row = 0
-                        type_leave_row = 0
-                        duration_row = 0
-                        start_date_row = 0
-                        end_date_row = 0
-                        for rownum in range(sheet.nrows):
-                            # _logger.info("-----------------------------------")
-                            # _logger.info(rownum)
-                            # # _logger.info(sheet.row_values(rownum))
-                            item = sheet.row_values(rownum)
-                            # _logger.info(item)
-                            # _logger.info("-----------------------------------")
-                            # if "رقم الموظف" in item and "نوع الإجازة " in item and "المدة " in item and "البداية " in item and "النهاية " in item:
+
+
+                    employee_info = self.env['hr.employee'].sudo().search([('id','=',1)])
+                    resource_calendar_id = employee_info.resource_calendar_id or self.env.company.resource_calendar_id
+                    _logger.info("resource_calendar_id")
+                    _logger.info(resource_calendar_id)
+                    
+                    # domain = [('calendar_id', '=', resource_calendar_id.id), ('display_type', '=', False)]
+                    # attendances = self.env['resource.calendar.attendance'].read_group(domain, ['ids:array_agg(id)', 'hour_from:min(hour_from)', 'hour_to:max(hour_to)', 'week_type', 'dayofweek', 'day_period'], ['week_type', 'dayofweek', 'day_period'], lazy=False)
+
+                    # # Must be sorted by dayofweek ASC and day_period DESC
+                    # attendances = sorted([DummyAttendance(group['hour_from'], group['hour_to'], group['dayofweek'], group['day_period'], group['week_type']) for group in attendances], key=lambda att: (att.dayofweek, att.day_period != 'morning'))
+
+                    # default_value = DummyAttendance(0, 0, 0, 'morning', False)
+            #         data_list = []
+            #         header_list = []
+            #         headers_dict = {}
+            #         for sheet in wb.sheets():
+            #             _logger.info(sheet.nrows)
+            #             first_row = False
+            #             emp_num_row = 0
+            #             type_leave_row = 0
+            #             duration_row = 0
+            #             start_date_row = 0
+            #             end_date_row = 0
+            #             for rownum in range(sheet.nrows):
+            #                 # _logger.info("-----------------------------------")
+            #                 # _logger.info(rownum)
+            #                 # # _logger.info(sheet.row_values(rownum))
+            #                 item = sheet.row_values(rownum)
+            #                 # _logger.info(item)
+            #                 # _logger.info("-----------------------------------")
+            #                 # if "رقم الموظف" in item and "نوع الإجازة " in item and "المدة " in item and "البداية " in item and "النهاية " in item:
                             
-                            check_emp_num = 0
-                            check_type_leave = 0
-                            check_duration = 0
-                            check_start_date = 0
-                            check_end_date = 0
-                            for idx1,item1 in enumerate(item):
-                                if str(item1).strip() == "رقم الموظف":
-                                    emp_num_row = idx1
-                                    check_emp_num = 1
-                                if str(item1).strip() == "نوع الإجازة":
-                                    type_leave_row = idx1
-                                    check_type_leave = 1
-                                if str(item1).strip() == "المدة":
-                                    duration_row = idx1 
-                                    check_duration = 1
-                                if str(item1).strip() == "البداية":
-                                    start_date_row = idx1 
-                                    check_start_date = 1
-                                if str(item1).strip() == "النهاية":
-                                    end_date_row = idx1
-                                    check_end_date = 1
-                                if  check_emp_num == 1 and  check_type_leave == 1 and  check_duration == 1 and check_start_date == 1 and  check_end_date == 1:     
-                                    first_row = rownum
-                            if  first_row != False:
-                                break    
-                        for rownum1 in range(sheet.nrows): 
-                            item_y = sheet.row_values(rownum1)           
-                            if rownum1 > first_row:
-                                _logger.info("----------------------------------")
-                                emp_num =  item_y[emp_num_row]
-                                type_leave = item_y[type_leave_row]
-                                duration = item_y[duration_row]
-                                start_date = item_y[start_date_row]
-                                end_date = item_y[end_date_row]
-                                if emp_num != "" and duration != "":
-                                    _logger.info(first_row)
-                                    _logger.info(int(emp_num))
-                                    _logger.info(type_leave)
-                                    _logger.info(int(duration))
-                                    valid_start_date = (start_date - 25569) * 86400.0
-                                    valid_start_date = datetime.datetime.utcfromtimestamp(valid_start_date)
-                                    valid_start_date = str(valid_start_date).split(' ')
-                                    valid_start_date = valid_start_date[0].replace("-","/",3)
-                                    _logger.info(valid_start_date)
-                                    valid_end_date = (end_date - 25569) * 86400.0
-                                    valid_end_date = datetime.datetime.utcfromtimestamp(valid_end_date)
-                                    valid_end_date = str(valid_end_date).split(' ')
-                                    valid_end_date = valid_end_date[0].replace("-","/",3)
-                                    _logger.info(valid_end_date)
-                                _logger.info("----------------------------------")
-            except Exception as e:
-                list_of_failed_record += str(e)
-                _logger.info("--------------------------------------------")
-                _logger.info("list_of_failed_record")
-                _logger.info(list_of_failed_record)
-                _logger.info("--------------------------------------------")
+            #                 check_emp_num = 0
+            #                 check_type_leave = 0
+            #                 check_duration = 0
+            #                 check_start_date = 0
+            #                 check_end_date = 0
+            #                 for idx1,item1 in enumerate(item):
+            #                     if str(item1).strip() == "رقم الموظف":
+            #                         emp_num_row = idx1
+            #                         check_emp_num = 1
+            #                     if str(item1).strip() == "نوع الإجازة":
+            #                         type_leave_row = idx1
+            #                         check_type_leave = 1
+            #                     if str(item1).strip() == "المدة":
+            #                         duration_row = idx1 
+            #                         check_duration = 1
+            #                     if str(item1).strip() == "البداية":
+            #                         start_date_row = idx1 
+            #                         check_start_date = 1
+            #                     if str(item1).strip() == "النهاية":
+            #                         end_date_row = idx1
+            #                         check_end_date = 1
+            #                     if  check_emp_num == 1 and  check_type_leave == 1 and  check_duration == 1 and check_start_date == 1 and  check_end_date == 1:     
+            #                         first_row = rownum
+            #                 if  first_row != False:
+            #                     break    
+            #             for rownum1 in range(sheet.nrows): 
+            #                 item_y = sheet.row_values(rownum1)           
+            #                 if rownum1 > first_row:
+            #                     _logger.info("----------------------------------")
+            #                     emp_num =  item_y[emp_num_row]
+            #                     type_leave = item_y[type_leave_row]
+            #                     duration = item_y[duration_row]
+            #                     start_date = item_y[start_date_row]
+            #                     end_date = item_y[end_date_row]
+            #                     if emp_num != "" and duration != "":
+            #                         _logger.info(first_row)
+            #                         _logger.info(int(emp_num))
+            #                         _logger.info(type_leave)
+            #                         _logger.info(int(duration))
+            #                         valid_start_date = (start_date - 25569) * 86400.0
+            #                         valid_start_date = datetime.datetime.utcfromtimestamp(valid_start_date)
+            #                         valid_start_date = str(valid_start_date).split(' ')
+            #                         valid_start_date = valid_start_date[0].replace("-","/",3)
+            #                         _logger.info(valid_start_date)
+            #                         valid_end_date = (end_date - 25569) * 86400.0
+            #                         valid_end_date = datetime.datetime.utcfromtimestamp(valid_end_date)
+            #                         valid_end_date = str(valid_end_date).split(' ')
+            #                         valid_end_date = valid_end_date[0].replace("-","/",3)
+            #                         _logger.info(valid_end_date)
+            #                     _logger.info("----------------------------------")
+            # except Exception as e:
+            #     list_of_failed_record += str(e)
+            #     _logger.info("--------------------------------------------")
+            #     _logger.info("list_of_failed_record")
+            #     _logger.info(list_of_failed_record)
+            #     _logger.info("--------------------------------------------")
 
 
         #     try:
@@ -214,3 +228,29 @@ class ImportHrLeave(models.Model):
         #         part_master.status = 'failed'
         #         _logger.error(e)
         #         self._cr.commit()
+
+
+
+
+
+
+    def _onchange_request_parameters(self):
+        if not self.request_date_from:
+            self.date_from = False
+            return
+
+        if self.request_unit_half or self.request_unit_hours:
+            self.request_date_to = self.request_date_from
+
+        if not self.request_date_to:
+            self.date_to = False
+            return
+
+        resource_calendar_id = self.employee_id.resource_calendar_id or self.env.company.resource_calendar_id
+        domain = [('calendar_id', '=', resource_calendar_id.id), ('display_type', '=', False)]
+        attendances = self.env['resource.calendar.attendance'].read_group(domain, ['ids:array_agg(id)', 'hour_from:min(hour_from)', 'hour_to:max(hour_to)', 'week_type', 'dayofweek', 'day_period'], ['week_type', 'dayofweek', 'day_period'], lazy=False)
+
+        # Must be sorted by dayofweek ASC and day_period DESC
+        attendances = sorted([DummyAttendance(group['hour_from'], group['hour_to'], group['dayofweek'], group['day_period'], group['week_type']) for group in attendances], key=lambda att: (att.dayofweek, att.day_period != 'morning'))
+
+        default_value = DummyAttendance(0, 0, 0, 'morning', False)
